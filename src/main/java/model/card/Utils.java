e04a68d2da34c09f819274ce842af676fdb100cb
shayan <shayanpardis82@gmail.com>
2021-05-06 15:29:48 +0430

    tiny

--- src/main/java/model/card/Utils.java
+++ src/main/java/model/card/Utils.java
@@ -2,12 +2,14 @@ package model.card;
 
 import model.card.monsterCards.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Utils {
     public static Card[] getAllCards(){
-        Card[] cards = {new AxeRaider(), new BattleOx(), new Fireyarou(), new HornImp(), new SilverFang()};
-        return cards;
+        return new Card[]{
+                new AxeRaider(),
+                new BattleOx(),
+                new Fireyarou(),
+                new HornImp(),
+                new SilverFang()
+        };
     }
 }
