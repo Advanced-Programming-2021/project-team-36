20e502d0b8a827b9d8767dba44aa37bae7572d91
Abolfazl <mr.abolfazl81@gmail.com>
2021-05-06 16:06:49 +0430

    MonsterState and MagicState added.

--- src/main/java/model/card/Magic.java
+++ src/main/java/model/card/Magic.java
@@ -1,7 +1,14 @@
 package model.card;
 
+import model.enums.MagicState;
+
 public class Magic extends Card {
+    protected MagicState magicState = null;
     protected Magic(String name, String description, int price) {
         super(name, description, price);
+    }
+
+    public MagicState getMagicState() {
+        return magicState;
     }
 }
