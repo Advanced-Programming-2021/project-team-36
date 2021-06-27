af2d510e6adc551366e30cac88f0261d6c0db2b8
shayan <shayanpardis82@gmail.com>
2021-06-28 00:01:41 +0430

    debug constant money string

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,7 @@ public class Main {
                         "press help when you needed help!\n" +
                         "also every thing will be saved if you \n" +
                         "exit the program by calling 'menu exit'\n" +
-                        "now you can login! We will give you " + Constants.InitialMoney + "$ go buy some cards for yourself!",
+                        "now you can login! We will give you " + Constants.InitialMoney.val + "$ go buy some cards for yourself!",
                 Color.Cyan
         );
     }
