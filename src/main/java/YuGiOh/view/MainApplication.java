58de94cdab9c55fd6d8eeb4f57510ae6e7fa6b9e
Kasra Mazaheri <Kasra.Mazaherii@gmail.com>
2021-07-07 23:03:30 +0430

    Fixed Database

--- src/main/java/YuGiOh/view/MainApplication.java
+++ src/main/java/YuGiOh/view/MainApplication.java
@@ -6,24 +6,36 @@ import YuGiOh.model.User;
 import YuGiOh.utils.Cheat;
 import YuGiOh.utils.DatabaseHandler;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class MainApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        initializeApp(primaryStage);
+
+        LoginMenuView.init(primaryStage);
+//        new User("ad", "Ad", "w8");
+//        new User("ad183", "nclad", "w8");
+//        new User("19", "1398d", "w8");
+//        new User("19831", "Adakd", "w8");
+        //User shayan  = new User("shayan", "shayan.p", "1234");
+        //Cheat.buildSuperUser(shayan);
+        //MainMenuView.init(primaryStage, shayan);
+    }
+
+    private void initializeApp(Stage primaryStage) {
         primaryStage.setResizable(false);
         primaryStage.setTitle("YuGiOh!");
         DatabaseHandler.importFromDatabase();
         new MediaPlayerController();
-
-        //LoginMenuView.init(primaryStage);
-        new User("ad", "Ad", "w8");
-        new User("ad183", "nclad", "w8");
-        new User("19", "1398d", "w8");
-        new User("19831", "Adakd", "w8");
-        User shayan  = new User("shayan", "shayan.p", "1234");
-        Cheat.buildSuperUser(shayan);
-        MainMenuView.init(primaryStage, shayan);
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                //DatabaseHandler.exportToDatabase();
+            }
+        });
     }
 }
