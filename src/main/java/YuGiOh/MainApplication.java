7c1b70aa31e46c847a07ea20c723488402a242cb
shayan <shayanpardis82@gmail.com>
2021-07-20 23:41:17 +0430

    gave up implementing game core in network. reverting everything to last working version. bye!

--- src/main/java/YuGiOh/MainApplication.java
+++ src/main/java/YuGiOh/MainApplication.java
@@ -17,17 +17,16 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
         initializeApp(primaryStage);
-
-//        LoginMenuView.init(primaryStage);
+        LoginMenuView.init(primaryStage);
 //        new User("ad", "Ad", "w8");
 //        new User("ad183", "nclad", "w8");
 //        new User("19", "1398d", "w8");
 //        new User("19831", "Adakd", "w8");
-        User shayan  = new User("shayan", "shayan.p", "1234");
-        User dummy = new User("dummy", "dum", "123");
-        Cheat.buildSuperUser(shayan);
-        Cheat.buildSuperUser(dummy);
-        MainMenuView.init(primaryStage, shayan);
+//        User shayan  = new User("shayan", "shayan.p", "1234");
+//        User dummy = new User("dummy", "dum", "123");
+//        Cheat.buildSuperUser(shayan);
+//        Cheat.buildSuperUser(dummy);
+//        MainMenuView.init(primaryStage, shayan);
     }
 
     private void initializeApp(Stage primaryStage) {
